b0f37d04bd8077fc575e3c730ea7858e708468ee
Dief Bell <dief.bell@stratasys.com>
2025-07-09 10:44:59 +0100

    Not using env file, hopefully have correct base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === "production" ? "https://diefbell-grabcad.github.io/gh-spa-test/" : "http://localhost:5173/",
   build: {
     outDir: 'docs/',
   },
